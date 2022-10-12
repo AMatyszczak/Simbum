8b4e1be4d55317eb8139b7f4941d8faf3d120c95
Adrian <adrianmatyszczak@gmail.com>
2022-10-12 20:29:43 +0200

    improved styling

--- renderer.js
+++ renderer.js
@@ -1,24 +1,16 @@
-document.addEventListener("drop", (event) => {
-  event.preventDefault();
-  event.stopPropagation();
+document.addEventListener('drop', (event) => {
+  event.preventDefault()
+  event.stopPropagation()
 
-  const file = event.dataTransfer.files.item(0);
-  if (file.type.includes("image/")) {
-    console.log("filepath:", file.path);
-    const imgElement = document.getElementById("dropped_image");
-    imgElement.src = file.path;
+  const file = event.dataTransfer.files.item(0)
+  if (file.type.includes('image/')) {
+    console.log('filepath:', file.path)
+    const imgElement = document.getElementById('dropped_image')
+    imgElement.src = file.path
   }
-});
+})
 
-document.addEventListener("dragover", (e) => {
-  e.preventDefault();
-  e.stopPropagation();
-});
-
-document.addEventListener("dragenter", (event) => {
-  console.log("File is in the Drop Space");
-});
-
-document.addEventListener("dragleave", (event) => {
-  console.log("File has left the Drop Space");
-});
+document.addEventListener('dragover', (e) => {
+  e.preventDefault()
+  e.stopPropagation()
+})
