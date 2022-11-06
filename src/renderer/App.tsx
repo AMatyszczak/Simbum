3aea8df2d688d7b4458c0d8ee9ae33175c40b613
Adrian <adrianmatyszczak@gmail.com>
2022-11-06 22:07:19 +0100

    added text initialization

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -6,9 +6,8 @@ import { LexicalComposer } from '@lexical/react/LexicalComposer';
 import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
 import { ContentEditable } from '@lexical/react/LexicalContentEditable';
 import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
-import { AutoScrollPlugin } from '@lexical/react/LexicalAutoScrollPlugin';
 import { AutoLinkNode } from '@lexical/link';
-import TabulateOnTabPressPlugin from 'plugins/TabulateOnTabPressPlugin';
+import { $createParagraphNode, $createTextNode, $getRoot } from 'lexical';
 import button_right from '../../assets/buttons/button_right.png';
 import button_left from '../../assets/buttons/button_left.png';
 import placeholder from '../../assets/img_placeholder.png';
@@ -16,19 +15,51 @@ import placeholder from '../../assets/img_placeholder.png';
 function Placeholder() {
   return <div className="album-image-description-content" />;
 }
-const Simbum = () => {
-  const editorConfig = {
+
+function populateEditor() {
+  const root = $getRoot();
+
+  const paragraphNode = $createParagraphNode();
+  const textNode = $createTextNode('Hello world');
+  paragraphNode.append(textNode);
+  root.append(paragraphNode);
+}
+
+function Editor() {
+  const initialConfig = {
     // The editor theme
     namespace: 'Simbumer',
     onError(error: any) {
       throw error;
     },
     nodes: [AutoLinkNode],
+    editorState: populateEditor,
   };
 
   return (
+    <div className="album-image-description-container">
+      <div className="album-image-description-scrollbar">
+        <LexicalComposer initialConfig={initialConfig}>
+          <RichTextPlugin
+            contentEditable={
+              <ContentEditable
+                id="Simbumer"
+                className="album-image-description-content"
+              />
+            }
+            placeholder={<Placeholder />}
+          />
+          <HistoryPlugin />
+        </LexicalComposer>
+      </div>
+    </div>
+  );
+}
+
+const Simbum = () => {
+  return (
     <div className="album-content">
-      <input className="album-title" type="text" value="Przyjaciele" />
+      <input className="album-title" type="text" defaultValue="Przyjaciele" />
       <div className="album-images-controller">
         <button className="previous-album-image-button" type="button">
           <img src={button_left} className="button-image" alt="" />
@@ -45,21 +76,7 @@ const Simbum = () => {
         </button>
       </div>
 
-      <div className="album-image-description-container">
-        <div className="album-image-description-scrollbar">
-          <LexicalComposer initialConfig={editorConfig}>
-            <RichTextPlugin
-              contentEditable={
-                <ContentEditable className="album-image-description-content" />
-              }
-              placeholder={<Placeholder />}
-            />
-            <HistoryPlugin />
-            <TabulateOnTabPressPlugin />
-            {/* <AutoScrollPlugin scrollRef={containerWithScrollRef} /> */}
-          </LexicalComposer>
-        </div>
-      </div>
+      <Editor />
     </div>
   );
 };
