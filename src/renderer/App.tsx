d268bb69d5635bf80e8bac4f9802d2f74565503a
Adrian <adrianmatyszczak@gmail.com>
2022-11-13 12:49:23 +0100

    style improvements. Added Quill in place of Lexical

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,51 +2,51 @@ import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 // import button_left_disabled from '../../assets/buttons/button_left_disabled.png'
 // import button_right_disabled from '../../assets/buttons/button_right_disabled.png'
 import './App.css';
-import { LexicalComposer } from '@lexical/react/LexicalComposer';
-import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
-import { ContentEditable } from '@lexical/react/LexicalContentEditable';
-import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
-import { AutoLinkNode } from '@lexical/link';
-import { $createParagraphNode, $createTextNode, $getRoot } from 'lexical';
+import { useState } from 'react';
+import ReactQuill from 'react-quill';
 import button_right from '../../assets/buttons/button_right.png';
 import button_left from '../../assets/buttons/button_left.png';
 import placeholder from '../../assets/img_placeholder.png';
-
-function populateDescriptionEditor() {
-  const root = $getRoot();
-
-  const paragraphNode = $createParagraphNode();
-  const textNode = $createTextNode('Hello world');
-  paragraphNode.append(textNode);
-  root.append(paragraphNode);
-}
+import 'react-quill/dist/quill.snow.css';
 
 function DescriptionEditor() {
-  const initialConfig = {
-    // The editor theme
-    namespace: 'Simbumer',
-    onError(error: any) {
-      throw error;
+  const [value, setValue] = useState('');
+
+  const modules = {
+    toolbar: false,
+    clipboard: {
+      matchVisual: false,
     },
-    nodes: [AutoLinkNode],
-    editorState: populateDescriptionEditor,
   };
+
+  const formats = [
+    'header',
+    'font',
+    'size',
+    'bold',
+    'italic',
+    'underline',
+    'strike',
+    'blockquote',
+    'list',
+    'bullet',
+    'indent',
+    'link',
+    'image',
+    'video',
+  ];
 
   return (
     <div className="album-image-description-container">
       <div className="album-image-description-scrollbar">
-        <LexicalComposer initialConfig={initialConfig}>
-          <RichTextPlugin
-            contentEditable={
-              <ContentEditable
-                id="Simbumer"
-                className="album-image-description-content"
-              />
-            }
-            placeholder={<div />}
-          />
-          <HistoryPlugin />
-        </LexicalComposer>
+        <ReactQuill
+          theme="snow"
+          value={value}
+          onChange={setValue}
+          modules={modules}
+          formats={formats}
+          className="album-image-description-content"
+        />
       </div>
     </div>
   );
@@ -65,13 +65,15 @@ const Simbum = () => {
         <button className="previous-album-image-button" type="button">
           <img src={button_left} className="button-image" alt="" />
         </button>
-        <img
-          id="dropped_image"
-          draggable="false"
-          className="album-image"
-          src={placeholder}
-          alt=""
-        />
+        <div className="album-image-container">
+          <img
+            id="dropped_image"
+            draggable="false"
+            className="album-image"
+            src={placeholder}
+            alt=""
+          />
+        </div>
         <button className="next-album-image-button" type="button">
           <img src={button_right} className="button-image" alt="" />
         </button>
